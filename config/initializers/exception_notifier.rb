b28ff86f1d6e776dfd8b2c8e44dcb06f01cf6691
Jean-Sébastien Ney <jney@jneysmacbook.local>
2008-03-13 22:07:55 +0100

    ok

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,2 +1,2 @@
-ExceptionNotifier.exception_recipients  = %w(jeansebastien.ney@gmail.com team@7theories.com)
+ExceptionNotifier.exception_recipients  = %w(team@7theories.com)
 ExceptionNotifier.sender_address        = %("Application Error" <app.error@horoscope-astrologique.fr>)
